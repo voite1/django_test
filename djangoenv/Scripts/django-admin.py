26589b942a124e09da5c51914c33ebc67e5240b9
Aleksey Kramer <voitel@gmail.com>
2015-02-21 08:30:20 -0800

    Initial Commit

---
+++ djangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Aleksey\My Documents\github\django_test\djangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
