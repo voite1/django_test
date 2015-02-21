414786797a38ed1d215e3c33cbbbd1e3491a0488
Aleksey Kramer <voitel@gmail.com>
2015-02-21 10:35:20 -0800

    Running first test failes

--- mysite/myblog/tests.py
+++ mysite/myblog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from myblog.models import Post
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    fixtures = ['myblog_test_fixture.json', ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+
+    def test_unicode(self):
+    	expected = "This is a title"
+    	p1 = Post(title=expected)
+    	actual = unicode(p1)
+    	self.assertEqual(expected, actual)
