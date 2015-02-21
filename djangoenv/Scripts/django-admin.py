bb3f736d90a7a810ab74c3cf120a8c475d17a885
Aleksey Kramer <voitel@gmail.com>
2015-02-21 08:39:22 -0800

    cleanup

--- djangoenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Aleksey\My Documents\github\django_test\djangoenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
